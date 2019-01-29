e7245f0f03bc54446ce26a0cebca3e11b966d6bf
Sven Weingartner <13959824+sven-communitales@users.noreply.github.com>
2019-01-29 19:54:48 +0100

    Initial commit including test

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# log
-Component to improve logging
+# Communitales Log Component
+
+Convenient integration for logging of messages and exceptions.
+
+Usage
+-----
+
+```
+class SomeClass
+{
+
+    use LogAwareTrait;
+
+    public function testLog(): void
+    {
+        // Log your message including debug information
+        $this->log('Test message', LogLevel::NOTICE, ['param1' => 'useful debug information']);
+    }
+
+    public function testLogException(): void
+    {
+        // Catch your exception
+        $exception = new RuntimeException('Something gone wrong');
+
+        // Log it with one line
+        $this->logException($exception);
+    }
+}
+
+```
---
+++ src/Log/LogAwareTrait.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * @copyright   Copyright (c) 2019 Communitales GmbH (https://www.communitales.com/)
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Communitales\Component\Log;
+
+use Psr\Log\LoggerAwareTrait;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LogLevel;
+
+/**
+ * Class LogAwareTrait
+ */
+trait LogAwareTrait
+{
+
+    use LoggerAwareTrait;
+
+    /**
+     * Log errors to the logger.
+     *
+     * @param string $message
+     * @param string $level
+     * @param array  $context
+     */
+    protected function log(string $message, string $level = LogLevel::ERROR, array $context = []): void
+    {
+        if ($this->logger instanceof LoggerInterface) {
+            $this->logger->log($level, $message, $context);
+        }
+    }
+
+    /**
+     * Log exception to the logger.
+     *
+     * @param \Throwable $exception
+     * @param string     $level
+     * @param array      $context
+     */
+    protected function logException(\Throwable $exception, string $level = LogLevel::ERROR, array $context = []): void
+    {
+        $context = array_merge($context, ['exception' => $exception, 'trace' => $exception->getTraceAsString()]);
+
+        $this->log($exception->getMessage(), $level, $context);
+    }
+
+}
